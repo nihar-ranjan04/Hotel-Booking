f7efde1034b1d0b6d84c98c995ea8dfd121d3c9b
Somaskandhan-lgtm <somaskandhan.s@openturf.in>
2025-09-23 18:30:40 +0530

    Testimonial Section,Newsletter Section,Footer Section,Rooms List Page,Filters has been completed

--- hotel-booking/src/App.jsx
+++ hotel-booking/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './Pages/Home';
+import Footer from './components/Footer';
+import AllRooms from './Pages/AllRooms';
 
 const App = () => {
 
@@ -14,8 +16,10 @@ const isOwnerPath = useLocation().pathname.includes("owner");
   <div className="min-h-[70vh]">
     <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='/rooms' element={<AllRooms/>} />
     </Routes>
     </div>
+    <Footer/>
       
     </div>
   )
--- hotel-booking/src/Pages/Home.jsx
+++ hotel-booking/src/Pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
 import ExclusiveOfferSection from '../components/ExclusiveOfferSection'
+import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
      <Hero /> 
      <FeaturedDestination />
      <ExclusiveOfferSection />
+     <Testimonial/>
+     <NewsLetter/>
     </>
   )
 }
